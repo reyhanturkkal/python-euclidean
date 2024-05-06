72717091aaf273d69721599fadea13976a210ae0
Reyhan Türkkal <94196332+reyhanturkkal@users.noreply.github.com>
2024-05-06 19:50:27 +0300

    Add files via upload

---
+++ euclid_calculation.py
@@ -0,0 +1,17 @@
+import math
+
+points = [(3, 7), (9, 2),(4,5),(1,6)]
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+print("Minimum distance between points:", min_distance)
